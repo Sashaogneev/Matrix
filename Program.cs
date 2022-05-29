f0a6cd9ce2fe26a0156e09e8669f3089d890795f
LENOVO <LENOVO@DESKTOP-V95PBUD>
2022-05-29 12:33:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace first1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo...");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("The Matrix has you...");
+            
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("Follow the white rabbit.");
+            
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            Console.WriteLine("Knock, knock, Neo.");
+
+            Thread.Sleep(3000);
+            Console.Clear();
+        }
+    } 
+        
+}
+
+
+
+
+
